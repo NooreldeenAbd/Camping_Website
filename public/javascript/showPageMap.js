7803f0cd808606f29203e255829bdd278a1ba790
nooreldeen <nooreldeen.osm@gmail.com>
2022-04-26 15:08:15 -0600

    fixed map bug

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,20 +1,20 @@
-const campInfo = JSON.parse(camp)
+const campInfo = camp
 
 mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/outdoors-v11',
-center: campInfo.geometry.coordinates,
+center: camp.geometry.coordinates,
 zoom: 10
 });
 
 
 const marker = new mapboxgl.Marker()
-    .setLngLat(campInfo.geometry.coordinates)
+    .setLngLat(camp.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({offset: 25})
-        .setHTML(`<p>${campInfo.title}</p>`)
+        .setHTML(`<p>${camp.title}</p>`)
     )
     .addTo(map);
 
